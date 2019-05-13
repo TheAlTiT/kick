7b45adc18147a0ad8f825293764c92d727a1c32c
thealtit <vstatinchuk@gmail.com>
2019-05-13 15:13:21 +0300

    make it better

--- src/main/resources/static/js/vuee.js
+++ src/main/resources/static/js/vuee.js
@@ -23,7 +23,7 @@ var vm = new Vue({
     methods: {
         fet: function () {
           //  fetch('http://localhost:9000/api/get/' + this.user.name + '?kolvo=' + this.dat + '&oppoName=' + this.oppo)
-            fetch('https://kickhard.herokuapp.com/api/get' + this.user.name + '?kolvo=' + this.dat + '&oppoName=' + this.oppo)
+            fetch('https://kickhard.herokuapp.com/api/get/' + this.user.name + '?kolvo=' + this.dat + '&oppoName=' + this.oppo)
                 .then(response => response.json())
                 .then((myJson) => {
                     this.info = JSON.stringify(myJson);
