d3d413b66c1fc686817f907b92dd39816308f92a
thealtit <vstatinchuk@gmail.com>
2019-05-13 19:30:22 +0300

    make it better

--- src/main/java/kicker/kick/repository/GameRepository.java
+++ src/main/java/kicker/kick/repository/GameRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
+
 public interface GameRepository extends PagingAndSortingRepository<Game, Long> {
     Page<Game> findAllByPlayerNameOrPlayer2Name(String name, String name2, Pageable pageable);
 
     @Query(value = "select g from Game as g  join  g.player as p where (p.name=:fname and p.oppoName=:oname) or (p.name=:oname and p.oppoName=:fname)")
     Page<Game> findPaging(@Param("fname") String name, @Param("oname") String name3, Pageable pageable);
+    @Transactional
     Game deleteAllByPlayerName(String name);
 }
